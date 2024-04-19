86764b571c54adbc3e5a8845b9689087fa6dcbe4
vinayak-sanketika <165653132+vinayak-sanketika@users.noreply.github.com>
2024-04-19 13:41:38 +0530

    Update app.py
    Working version of the file commit

--- app.py
+++ app.py
@@ -1,67 +1,64 @@
 from confluent_kafka import Producer, Consumer
-from confluent_kafka.admin import NewTopic,AdminClient
 import json
 
-admin_client = AdminClient({
-     "bootstrap.servers": "localhost:9092"
-     })
+
+# path="/usr/local/kafka/appKafka/intents.json"
+# msg = read_file(path)
+# print("msg:",msg)
+
+
 
 class kafakapp:
     def __init__(self, config):
             self.config = config
             self.producer = Producer(self.config)
             self.consumer = Consumer(self.config)
-            
-        
-    def create_topic(self,source_topic,target_topic):
-        topic_list = []
-        topic_list.append(NewTopic(source_topic, 3, 1))
-        topic_list.append(NewTopic(target_topic, 3, 1))
-        admin_client.create_topics(topic_list)
-        print("-------Created the topics--------")
-
         
 
     def sendMessage(self,topic,message):
             self.producer.produce(topic,message.encode())
             self.producer.flush()
 
-            print(f"-----Message are sent to the {topic} topic------")
-
-    def consumerMessage(self, sourceTopic, targetTopic):
-        print("Inside the consumerMessage")
-        self.consumer.subscribe([sourceTopic, targetTopic])
-            
-        while True:
-            message = self.consumer.poll(1.0)
-            if message is None:
-                continue
-            if message.error():
-                print("Consumer error:", message.error())
-                continue
-            #from obj to dict
-            data = json.loads(message.value().decode())
-            #print("Data type:", type(data))
-            print("Received Message from Source:", json.dumps(data))
-            
-            
-            if message.topic() == sourceTopic:
-                #to sting from dict
-                self.sendMessage(targetTopic, json.dumps(data))
-            elif message.topic() == targetTopic:
+    def consumerMessage(self,topic):
+            self.consumer.subscribe([topic])
                 
-                print("Received Message from Target:", json.dumps(data))
+            while True:
+                    message=self.consumer.poll(1.0)
+                    if message is None:
+                        continue
+                    if message.error():
+                            print("Consumer error:", message.error())
+                            continue
+                    data=json.loads(message.value().decode())
+                    print("data tyep",type(data))
+                    print("Received Message :",json.dumps(data))
+                    self.sendMessage("target",json.dumps(data))
+                    #self.targetMessage("target")
 
-    
+    # def targetMessage(self,targetTopic):
+    #         self.consumer.subscribe([targetTopic])
+                
+    #         while True:
+    #                 message=self.consumer.poll(1.0)
+    #                 if message is None:
+    #                     continue
+    #                 if message.error():
+    #                     print("Consumer error:", message.error())
+    #                     continue
+    #                 data=json.loads(message.value().decode())
+    #                 print("data tyep",type(data))
+    #                 print("Target Message :",json.dumps(data))
+
 if __name__ == "__main__":
     config={
         'bootstrap.servers':'localhost:9092',
         'group.id':'my-group-new',
-        #'auto.offset.reset': 'earliest'
+        'auto.offset.reset': 'earliest'
           }
     app=kafakapp(config)
-    app.create_topic('source','target')
 
+#     msg = read_file("/usr/local/kafka/appKafka/intents.json")
+#     json.dumps(msg)
     msg_json = "/usr/local/kafka/appKafka/data.json"
     json_list=[]
     with open (msg_json) as file:
@@ -76,6 +73,6 @@ if __name__ == "__main__":
         # print(type(items))
         # print("items:",items)
         app.sendMessage('source',items)
+    #app.sendMessage('first',msg)
 
-    app.consumerMessage('source','target')
-    
+    app.consumerMessage('source')
